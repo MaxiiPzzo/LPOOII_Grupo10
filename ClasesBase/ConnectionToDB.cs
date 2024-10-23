81725b2fd46320fdafa04e53beef2ffe57a9eceb
fernandasivila <fernandabsivila@gmail.com>
2024-10-23 17:44:08 -0300

    Creación y Conexion de la BD

---
+++ ClasesBase/ConnectionToDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ClasesBase
+{
+    public abstract class ConnectionToDB
+    {
+        private readonly SqlConnection _connectionString;
+
+        public ConnectionToDB()
+        {
+            string connectionString = ClasesBase.Properties.Settings.Default.DefaultConnection;
+
+            _connectionString = new SqlConnection(connectionString);
+
+        }
+
+        protected SqlConnection getConnection()
+        {
+            return _connectionString;
+        }
+    }
+}
